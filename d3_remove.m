cb970e164f13e9f89459dc97423c0ce9f2e367d8
schittarath3 <souliya3chittarath@gmail.com>
2020-05-23 02:00:12 -0400

    bwconncomp 16 check

--- d3_remove.m
+++ d3_remove.m
@@ -1,12 +1,30 @@
-clear
 clc
+clear
 
+im1 = imread("D:\VIP Concrete\Example\3. Binary Images\bottom_left\bl_top0076_post.png");
+im2 = imread("D:\VIP Concrete\Example\3. Binary Images\bottom_left\bl_top0077_post.png");
+im3 = imread("D:\VIP Concrete\Example\3. Binary Images\bottom_left\bl_top0078_post.png");
 %create psuedo binary image
-mat1 = [1 1 0 0; 1 1 0 0; 0 0 0 0; 0 0 0 0];
-mat2 = [0 0 0 0; 0 0 0 0; 0 0 1 1; 0 0 1 1];
-mat3 = [0 0 0 0; 0 0 0 0; 0 0 1 1; 0 0 1 1];
+% mat1 = [1 1 0 0; 1 1 0 0; 0 0 0 0; 0 0 0 0];
+% mat2 = [0 0 0 0; 0 0 0 0; 0 0 1 1; 0 0 1 1];
+% mat3 = [0 0 0 0; 0 0 0 0; 0 0 1 1; 0 0 1 1];
 
 %create 3D matrix
-mat3D = cat(3, mat1, mat2, mat3);
+mat3D = cat(3, im1, im2, im3);
 
-CC = bwconncomp(mat3D, 6);
+CC = bwconncomp(mat3D, 18);
+
+pixels = CC.PixelIdxList;
+
+a = size(CC.PixelIdxList);
+num_clusters = a(2);
+
+for cluster = 1:num_clusters
+    if cluster == 36
+        continue
+    end
+    for i = 1:length(pixels{1, cluster})
+        mat3D(pixels{1, cluster}(i)) = 0;
+    end
+end
+

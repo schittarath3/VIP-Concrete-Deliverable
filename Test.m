fdaf1b8819590e3a83846d12184145f998c6f9c4
Vi Vo <vituongvoh@gmail.com>
2020-05-28 20:47:13 -0500

    edit pullfiles with meshvolume

--- Test.m
+++ Test.m
@@ -2,54 +2,56 @@ clc
 clear
 
 %% Generating Repository of Aggregates
-%Load in all of the aggregates and shrink them by percentage (set this as a
-%variable so we can experiment later) - to shrink, use the scale matrix..
-original = 20; %Number of aggregates that must be contained inside the box
-
-%s = .90; %Shrink so it is .9 of original size
+%Load in all of the aggregates
+pullFiles
+for i = 1:samples
+    structname = strcat('agg',num2str(i));
+    repos.(structname).Points = aggList{i}.Points;
+    repos.(structname).ConnectivityList = aggList{i}.ConnectivityList;
+end
 
 %Mesh Repository: [x y z connectivitylist volumefraction l w h];
-numagg = 41; %Number of aggregates
-mesh_opt = [];
-for aggregate = 1:numagg
-    %Define the volume here
-    %Vmesh
-    for alpha = linspace(-pi/12,pi/12)
-        for beta = linspace(-pi/12,pi/12)
-            for gamma = linspace(-pi/12,pi/12)
-                %Normalize here...
-
-                rotx = [1 0 0 0; 0 cos(alpha) -sin(alpha) 0; 0 sin(alpha) cos(alpha) 0; 0 0 0 1];
-                roty = [cos(beta) 0 sin(beta) 0; 0 1 0 0; -sin(beta) 0 cos(beta) 0; 0 0 0 1];
-                rotz = [cos(gamma) -sin(gamma) 0 0; sin(gamma) cos(gamma) 0 0; 0 0 1 0; 0 0 0 1];
-                R = rotx*roty*rotz;
-
-                s = 1; %Keep original shrunken size
-                tx = 0; ty = 0; tz = 0; %Keep object at origin
-                S = [s 0 0 0; 0 s 0 0; 0 0 s 0; 0 0 0 1];
-                T = [1 0 0 tx; 0 1 0 ty; 0 0 1 tz; 0 0 0 1];
-
-                TR = R*S*T;
-
-                %new matrix = old matrix (create new matrix)
-                %for loop here, replace the coordinates with new coordinates
-                %after transformation...
-                %datapoints - the new set of coordinates
-                [Vbox, l, w, h] = get.VolBox(datapoints);
-                
-                %The volume fraction (Volume of Mesh/Volume of Box)...
-                volfrac = Vmesh/Vbox;
-            end %stop for rotation angles in x
-        end% stop for rotation angles in y
-    end% stop for rotation angles in z
-end% stop with all aggregates
+% numagg = 41; %Number of aggregates
+% mesh_opt = [];
+% for aggregate = 1:numagg
+%     %Define the volume here
+%     %Vmesh
+%     for alpha = linspace(-pi/12,pi/12)
+%         for beta = linspace(-pi/12,pi/12)
+%             for gamma = linspace(-pi/12,pi/12)
+%                 %Normalize here...
+% 
+%                 rotx = [1 0 0 0; 0 cos(alpha) -sin(alpha) 0; 0 sin(alpha) cos(alpha) 0; 0 0 0 1];
+%                 roty = [cos(beta) 0 sin(beta) 0; 0 1 0 0; -sin(beta) 0 cos(beta) 0; 0 0 0 1];
+%                 rotz = [cos(gamma) -sin(gamma) 0 0; sin(gamma) cos(gamma) 0 0; 0 0 1 0; 0 0 0 1];
+%                 R = rotx*roty*rotz;
+% 
+%                 s = 1; %Keep original shrunken size
+%                 tx = 0; ty = 0; tz = 0; %Keep object at origin
+%                 S = [s 0 0 0; 0 s 0 0; 0 0 s 0; 0 0 0 1];
+%                 T = [1 0 0 tx; 0 1 0 ty; 0 0 1 tz; 0 0 0 1];
+% 
+%                 TR = R*S*T;
+% 
+%                 %new matrix = old matrix (create new matrix)
+%                 %for loop here, replace the coordinates with new coordinates
+%                 %after transformation...
+%                 %datapoints - the new set of coordinates
+%                 [Vbox, l, w, h] = get.VolBox(datapoints);
+%                 
+%                 %The volume fraction (Volume of Mesh/Volume of Box)...
+%                 volfrac = Vmesh/Vbox;
+%             end %stop for rotation angles in x
+%         end% stop for rotation angles in y
+%     end% stop for rotation angles in z
+% end% stop with all aggregates
 
 %OUTPUT: A matrix with all of the aggregates ID, include properties: Mesh
 %Volume, Box Volume (for packing), Width, Length, and Height (dim of box
 %volume)
 
 %% FUNCTION 1: Normalize Each Aggregate
-function datapoints = get.Normalize(datapoints)
+function datapoints = normalize(datapoints)
 %Obtain the center of each aggregate
 x = datapoints(:,1);
 y = datapoints(:,2);
@@ -69,21 +71,19 @@ end
 end
 
 %% FUNCTION 2: Generating the Volume of Each Aggregate
-function [Vbox, l, w, h] = get.VolBox(datapoints)
+function [Vbox, l, w, h] = getVolBox(datapoints)
 %Input: datapoints - written as column vectors [x y z];
 %Output: V - the volume of the container/box
 %            l, w, h - dimensions of the box (length, width, height)
 w = abs(max(datapoints(:,1)) - min(datapoints(:,1)));
 l = abs(max(datapoints(:,2)) - min(datapoints(:,2)));
-h = abs(max(datapoints(:,3)) - min(datapoints(:,3));
+h = abs(max(datapoints(:,3)) - min(datapoints(:,3)));
 Vbox = w.*l.*h;
 end
 
-function Vmesh = get.VolMesh(filename)
+function Vmesh = getVolMesh(filename)
 model = createpde;
 importGeometry(model,filename);
 mesh = generateMesh(model);
 Vmesh = volume(mesh);
 end
-
-%% PACKING THE AGGREGATES
--- pullFiles.m
+++ pullFiles.m
@@ -2,11 +2,19 @@
 fileList = dir('STL Files\Aggregates Processed\*.stl');
 fileList = struct2cell(fileList);
 aggList = cell(1, length(fileList));
-for i = 1:length(fileList)
+samples = length(fileList);
+meshvol = zeros(1,samples);
+for i = 1:samples
+    %obtain stl
     folder = fileList{2, i};
     file = fileList{1, i};
     fn = strcat(folder, '\', file);
-    aggList{i} = stlread(fn)
-end
-
+    aggList{i} = stlread(fn);
     
+    %obtain mesh volume
+    i %for debugging (:
+    model = createpde;
+    importGeometry(model,fn);
+    mesh = generateMesh(model);
+    meshvol(i) = volume(mesh);
+end

61abf8123a519c62c9a666b5f24eeb31a21cb00c
Souliya <souliya3chittarath@gmail.com>
2020-06-10 02:33:28 -0400

    idek

--- growAgg.m
+++ growAgg.m
@@ -1,22 +1,25 @@
-function growAgg(aggRepo, cubeCell, scaleStepFactor)
+function newRepo = growAgg(aggRepo, cubeCell, scaleStepFactor)
     aggNames = fieldnames(aggRepo);
     numAgg = length(aggNames);
     scaleUp = [scaleStepFactor 0 0; 0 scaleStepFactor 0; 0 0 scaleStepFactor];
     scaleDown = [1/scaleStepFactor 0 0; 0 1/scaleStepFactor 0; 0 0 1/scaleStepFactor];
     
+    newRepo = struct
     for i = 1:numAgg
-        curAggName = aggNames{i}
+        curAggName = aggNames{i};
         curCubeNum = aggRepo.(curAggName).cubeNum;
         curCubePoints = cubeCell{curCubeNum};
         curCubeAlpha = alphaShape(curCubePoints(:,:,:));
-        numTries = 1
+        numTries = 1;
+        newRepo.(curAggName).cubeNum = curCubeNum;
+        newRepo.(curAggName).Points = aggRepo.(curAggName).Points
         while 1
-            aggRepo.(curAggName).Points = ...
-                aggRepo.(curAggName).Points  * scaleUp
-            pointCheck = ~inShape(curCubeAlpha, aggRepo.(curAggName).Points)
+            newRepo.(curAggName).Points = ...
+                newRepo.(curAggName).Points  * scaleUp;
+            pointCheck = ~inShape(curCubeAlpha, newRepo.(curAggName).Points);
             if pointCheck > 0
-                aggRepo.(curAggName).Points = ...
-                    aggRepo.(curAggName).Points * scaleDown
+                newRepo.(curAggName).Points = ...
+                    newRepo.(curAggName).Points * scaleDown;
                 disp(numTries)
                 break
             end

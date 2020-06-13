b6b8a8e162d82fb1f9ae2228bc821d5d19d01b21
Vi Vo <vituongvoh@gmail.com>
2020-06-13 13:23:52 -0500

    fix tangent plane code

--- tangentPlane.m
+++ tangentPlane.m
@@ -1,65 +1,137 @@
-function TPlane = tangentPlane(filename,restrictface)
-%Input: filename - the stl file of the aggregate
-%           restrictface - the section of the aggregate to generate the
-%           tangent plane (1 - bottom left, 2 - bottom right, 3 - top left,
-%           4 - top right) 
-%Output: the goal of this function is to generate a visual representation
-%of the tangent plane along with the plane equation in the form of Ax + By
-%+ Cz + D = 0 (written as coefficients).
+tangentPlane.m
+clc
+clear
+
+function plotaggregate(agg2,agg1,rotangle,NormVec,option)
+pts2 = normalize(stlread(agg2).Points);
+if ~isempty(rotangle)
+pts2 = Rotate(pts2,0,rotangle,0);
+end
+pts1 = normalize(stlread(agg1).Points);
+
+if ~isempty(NormVec)
+pts2 = Translate(pts2,pts1,NormVec,option);
+end
+cnt = stlread(agg2).ConnectivityList;
+trimesh(cnt,pts2(:,1),pts2(:,2),pts2(:,3),'FaceAlpha',0,'EdgeColor','k');
+hold on
+end
+
+function result = optAngle(agg1,agg2,restrictface1,restrictface2)
+%Inputs: agg1 - the stationary/fixed aggregate
+%             agg2 - the aggregate that needs to be "attached"/shifted
+a = linspace(0,pi./2);
+optimalangle = [];
+plane1 = tangentPlane(agg1,restrictface1,0,false);
+
+parfor ang = 1:length(a)
+    plane2 = tangentPlane(agg2,restrictface2,a(ang),false);
+    radt = anglebwPlanes(plane1,plane2);
+    optimalangle = [optimalangle; radt];
+end
+
+[~,idx] = min(optimalangle);
+result = a(idx);
+end
+
+function result = anglebwPlanes(normvec1,normvec2)
+magNormVec1 = norm(normvec1(1:3));
+magNormVec2 = norm(normvec2(1:3));
+
+result = acos(dot(normvec1(1:3),normvec2(1:3))./(magNormVec1.*magNormVec2));
+end
+
+function result = tangentPlane(filename,option,angle,figurecheck)
+switch option
+    case 1 %LEFT
+        split1 = 1; %axis where the aggregate is cut into halves
+        split2 = 3; %axis where the aggregate is cut into halves again
+        split3 = 2;
+        restrictface = 1;
+    case 2 %RIGHT
+        split1 = 1;
+        split2 = 3;
+        split3 = 2;
+        restrictface = 4;
+    case 3 %BOTTOM
+        split1 = 3;
+        split2 = 2;
+        split3 = 1;
+        restrictface = 2;
+    case 4 %TOP
+        split1 = 3;
+        split2 = 2;
+        split3 = 1;
+        restrictface = 4;
+end
+
 pts1 = normalize(stlread(filename).Points);
+pts1 = Rotate(pts1,0,angle,0);
 
 %generating the tangent line of the top...
-[~,idx] = sort(pts1(:,3));
+[~,idx] = sort(pts1(:,split1));
 sortedpts1 = pts1(idx,:);
-negidx = find(sortedpts1(:,3) <= 0); %gathering all the points below z=0
-if restrictface ==1 || restrictface == 2 %below Z=0
-    newpts1 = sortedpts1(1:negidx(end,1),:); %forming new pts below z=0
+negidx = find(sortedpts1(:,split1) <= 0); 
+if restrictface ==1 || restrictface == 2 
+    newpts1 = sortedpts1(1:negidx(end,1),:); 
 else
-    newpts1 = sortedpts1(negidx(end,1)+1:end,:); %forming new pts above z=0
+    newpts1 = sortedpts1(negidx(end,1)+1:end,:);
 end
 
 %finding if there are any flat faces
-lowz = unique(round(newpts1(:,3),3));
-lowzidx = find(newpts1(:,3) > lowz(1));
+lowz = unique(round(newpts1(:,split1),3));
+lowzidx = find(newpts1(:,split1) > lowz(1));
 
 %removing flat face to generate representative plane...
 %rotating the aggregate until the min x is more than 0...
 flatface = newpts1(1:lowzidx-1,:);
 
-if length(flatface) > 10 %needs at least 10 points for flat face
+if length(flatface) > 100
 dTheta = 1; 
 stepSz = .25;
 if ~isempty(flatface) %there is flat face
-    while min(flatface(:,1)) < 0
+    while min(flatface(:,split2)) < 0
         dTheta = dTheta + stepSz;
         flatface = Rotate(flatface,0,0,-pi./dTheta);
     end
     pts1 = Rotate(pts1,0,0,-pi/dTheta);
     newpts1 = Rotate(newpts1,0,0,-pi/dTheta);
 end
+    flat = true;
+else
+    flat = false;
 end
-cnt1 = stlread(filename).ConnectivityList;
-figure
-mesh1 = trimesh(cnt1,pts1(:,1),pts1(:,2),pts1(:,3));
-mesh1.FaceAlpha = 1;
-mesh1.EdgeColor = 'b';
-xlabel('x')
-ylabel('y')
-zlabel('z')
-axis equal
-hold on
+
+if figurecheck == true
+    figure
+    cnt1 = stlread(filename).ConnectivityList;
+    mesh1 = trimesh(cnt1,pts1(:,1),pts1(:,2),pts1(:,3));
+    mesh1.FaceAlpha = 1;
+    mesh1.EdgeColor = 'b';
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+    axis equal
+    hold on
+end
 
 %which tangent plane needs to be find depends on the region...
-[~,idx] = sort(newpts1(:,1));
-sortedpts2 = newpts1(idx,:);
-negidx = find(sortedpts2(:,1) <= 0);
-    if restrictface == 1 || restrictface == 3
-    newpts1 = sortedpts2(1:negidx(end,1)-1,:);
-    else
-    newpts1 = sortedpts2(negidx(end,1)+1:end,:);
-    end
-plot3(newpts1(:,1),newpts1(:,2),newpts1(:,3),'.r')
-hold on
+if flat == true && restrictface == 2
+    newpts1 = flatface;
+else
+    [~,idx] = sort(newpts1(:,split2));
+    sortedpts2 = newpts1(idx,:);
+    negidx = find(sortedpts2(:,split2) <= 0);
+        if restrictface == 1 || restrictface == 3
+        newpts1 = sortedpts2(1:negidx(end,1)-1,:);
+        else
+        newpts1 = sortedpts2(negidx(end,1)+1:end,:);
+        end
+end
+if figurecheck == true
+    plot3(newpts1(:,1),newpts1(:,2),newpts1(:,3),'.r')
+    hold on
+end
 
 %calculating the centroid
 x = newpts1(:,1);
@@ -75,56 +147,63 @@ r0 = [xcm ycm zcm];
 order = abs(newpts1(:,1:2)-r0(1:2));
 [~,minidx] = min(order(:,1) + order(:,2));
 r0 = newpts1(minidx,:);
-plot3(r0(1),r0(2),r0(3),'.','MarkerSize',50)
-hold on
+% plot3(r0(1),r0(2),r0(3),'.','MarkerSize',50)
+% hold on
 
 %writing the equation for tangent plane...
 %finding the point in the same x-axis (relatively)
 ptsX = [];
 for i = 1:length(newpts1)
-    difX= newpts1(i,1) - r0(1);
-    if abs(difX) < 8
+    difX= newpts1(i,split2) - r0(split2);
+    if abs(difX) < 15
         term = newpts1(i,:);
         ptsX = [ptsX; term];
     end
 end
 %obtaining the vector with the most difference in Y
-[~,maxY] = max(ptsX(:,2));
-[~,minY] = min(ptsX(:,2));
+[~,maxY] = max(ptsX(:,split3));
+[~,minY] = min(ptsX(:,split3));
 WidV = ptsX(maxY,:) - ptsX(minY,:);
 
 %finding the point in the same y-axis (relatively)
 ptsY = [];
 for i = 1:length(newpts1)
-    difY= newpts1(i,2) - r0(2);
-    if abs(difY) < 8
+    difY= newpts1(i,1) - r0(split3);
+    if abs(difY) < 15
         term = newpts1(i,:);
         ptsY = [ptsY; term];
     end
 end
-DepV = ptsY(end,:) - ptsY(1,:);
+DepV = ptsY(end,:) - ptsY(split2,:);
 
 Normvec = cross(DepV,WidV);
 Normvec = Normvec./norm(Normvec);
-[X,Y] = meshgrid(-45:2:45);
+
+%limit x and y to only the max width/length
+limxy = abs([max(newpts1(:,1)), max(newpts1(:,2)), min(newpts1(:,1)), min(newpts1(:,2))]);
+limxy = max(limxy);
+
+[X,Y] = meshgrid(-limxy:2:limxy);
 %move r0 so that the z is now zmin...
 if restrictface == 1 || restrictface == 2
-    [~,minZ] = min(newpts1(:,3));
+    [~,minZ] = min(newpts1(:,split1));
     r0 = newpts1(minZ,:); %readjust to include the minimal point in plane
 else
-    [~,maxZ] = max(newpts1(:,3));
+    [~,maxZ] = max(newpts1(:,split1));
     r0 = newpts1(maxZ,:);
 end
 D = -1*(Normvec(1)*r0(1) + Normvec(2)*r0(2) + Normvec(3)*r0(3));
 Z = -1/Normvec(3)*(Normvec(1)*X + Normvec(2)*Y + D);
+if figurecheck == true
 surf(X,Y,Z,'FaceColor','w');
 hold off
 axis equal
-
-TPlane = [Normvec(1), Normvec(2), Normvec(3), D];
 end
 
-function datapointsn = normalize(datapoints)
+result = [Normvec, D];
+end
+
+function [datapointsn, centroid] = normalize(datapoints)
 %Obtain the center of each aggregate
 x = datapoints(:,1);
 y = datapoints(:,2);
@@ -140,6 +219,42 @@ datapointsn = datapoints;
 dcm = [0, 0, 0] - centroid; %Distance from centroid to (0,0,0)
 for vertice = 1:length(datapoints)
     datapointsn(vertice,:) = datapoints(vertice,:) + dcm;
+end
+end
+
+function datapointsn = Translate(datapoints1,datapoints2,Normvec,option)
+switch option
+    case 1
+        scalar = -1;
+    case 2
+        scalar = 1;
+    case 3
+        scalar = 1;
+    case 4
+        scalar = -1;
+end
+
+agg2 = alphaShape(datapoints2); %stationary
+
+alpha = 0;
+StepSz = 1;
+datapointsn = datapoints1;
+
+locate = sum(inShape(agg2,datapoints1));
+while locate~=0
+    Normvecn = scalar*Normvec.*(alpha + StepSz);
+
+    datapointsn(:,1) = datapointsn(:,1) + Normvecn(1);
+    datapointsn(:,2) = datapointsn(:,2) + Normvecn(2);
+    datapointsn(:,3) = datapointsn(:,3) + Normvecn(3);
+    locate = sum(inShape(agg2,datapointsn));
+
+    if locate ~=0
+        datapointsn = datapoints1; %reset
+        StepSz = StepSz + 1;
+    else
+        break
+    end
 end
 end
 

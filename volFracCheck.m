6331f377b4773bac218af6290eb78bae2158d8d0
Souliya <souliya3chittarath@gmail.com>
2020-06-14 17:17:32 -0400

    ok this one actually works

--- volFracCheck.m
+++ volFracCheck.m
@@ -1,19 +1,40 @@
-function volFract = volFracCheck(aggRepo, cubeLength)
+function volumeFrac = volFracCheck(aggRepo, cubeLength)
 %Gets volume fraction of aggregate within the cube
 %Inputs: 
 %   aggRepo: Struct of aggregates of form aggName ->cubeNum,Faces,Points
 %   cubeLength: Length of cube
 %Outputs:
-%   Volume fraction of aggregates within cube as a float
+%   Volume fraction of aggregates within cube as float value
     cubeVol = cubeLength^3; %Volume of cube
     aggNames = fieldnames(aggRepo);
     numAgg = length(aggNames);
     totalAggVol = 0;
-    for i = 1:numAgg %Adds the volume of aggregate alphashapes
-        curAgg = aggRepo.(aggNames{i}).Points;
-        aggAlpha = alphaShape(curAgg);
-        aggVol = volume(aggAlpha);
-        totalAggVol = totalAggVol + aggVol;
+    for i = 1:numAgg %calculate volume for each aggregate and totals
+        p = aggRepo.(aggNames{i}).Points';
+        f = aggRepo.(aggNames{i}).Faces';
+        [volume, area] = stlVolume(p,f);
+        totalAggVol = totalAggVol + volume;
     end
-    volFract = totalAggVol/cubeVol;
+    volumeFrac = -totalAggVol/cubeVol;
+end
+
+function [totalVolume,totalArea] = stlVolume(p,t)
+% Given a surface triangulation, compute the volume enclosed using
+% divergence theorem.
+% Assumption:Triangle nodes are ordered correctly, i.e.,computed normal is outwards
+% Input: p: (3xnPoints), t: (3xnTriangles)
+% Output: total volume enclosed, and total area of surface  
+% Author: K. Suresh; suresh@engr.wisc.edu
+
+% Compute the vectors d13 and d12
+d13= [(p(1,t(2,:))-p(1,t(3,:))); (p(2,t(2,:))-p(2,t(3,:)));  (p(3,t(2,:))-p(3,t(3,:)))];
+d12= [(p(1,t(1,:))-p(1,t(2,:))); (p(2,t(1,:))-p(2,t(2,:))); (p(3,t(1,:))-p(3,t(2,:)))];
+cr = cross(d13,d12,1);%cross-product (vectorized)
+area = 0.5*sqrt(cr(1,:).^2+cr(2,:).^2+cr(3,:).^2);% Area of each triangle
+totalArea = sum(area);
+crNorm = sqrt(cr(1,:).^2+cr(2,:).^2+cr(3,:).^2);
+zMean = (p(3,t(1,:))+p(3,t(2,:))+p(3,t(3,:)))/3;
+nz = -cr(3,:)./crNorm;% z component of normal for each triangle
+volume = area.*zMean.*nz; % contribution of each triangle
+totalVolume = sum(volume);%divergence theorem
 end

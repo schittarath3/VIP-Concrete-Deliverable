94e044d7eb9daca659100ec1165201ad8b81b241
Vi Vo <vituongvoh@gmail.com>
2020-06-18 03:15:02 -0500

    add the repository and prototype of first aggregate packing alogirthm

--- tangentPlane.m
+++ tangentPlane.m
@@ -1,9 +1,108 @@
 clc
 clear
 
+repos = load('testingagg.mat');
+fields = fieldnames(repos.myRepo);
+angles = linspace(-pi/8,pi/8,5);
+
+%create a matrix to index each of the aggregate...
+order = ones(3,3);
+iter = 0; multiplier = length(fields).^(1/3);
+for j = 1:multiplier
+    for i = 1:multiplier
+        for k = 1:multiplier
+            iter = iter + 1;
+            order(k,i,j) = iter;
+        end
+    end
+    order(:,:,j) = rot90(order(:,:,j),2);
+end
+
+%generating the columns of each aggregates
+for col = 1:3
+    for agg = 1:3
+        ao = order(1:3,agg,col);
+        ptsn1 = normalize(repos.myRepo.(fields{ao(3)}).Points);
+        ptsn2 = normalize(repos.myRepo.(fields{ao(2)}).Points);
+        ptsn3 = normalize(repos.myRepo.(fields{ao(1)}).Points);
+        
+        cntn1 = repos.myRepo.(fields{ao(3)}).Faces;
+        cntn2 = repos.myRepo.(fields{ao(2)}).Faces;
+        cntn3 = repos.myRepo.(fields{ao(1)}).Faces;
+        
+        rottop = optAngle(ptsn2,ptsn1,'bottom','top',2);
+            Normvec = tangentPlane(ptsn1,cntn1,'top',[0 rottop 0],false);
+            newpts = [plotaggregate(ptsn1,ptsn2,cntn1,[0 rottop 0],Normvec,'top',true); ptsn2];
+            hold on
+            
+        rotbot = optAngle(ptsn2,ptsn3,'top','bottom',2);
+            Normvec = tangentPlane(ptsn3,cntn3,'bottom',[0 rotbot 0],false);
+            newpts = [plotaggregate(ptsn3,newpts,cntn3,[0 rotbot 0],Normvec,'bottom',true); newpts];
+            hold on
+            
+        plotaggregate(ptsn2,[],cntn2,[],[],[],true);
+            axis equal
+        hold off
+        
+        aggStacks(:,:,ao(1)./3) = newpts;
+        aggCon(:,:,ao(1)./3) = [cntn3; cntn1+102; cntn2+102*2];
+    end
+end
+
+%generating each of the layers...
+for layer = 1:3
+    stackidx = order(:,layer,1);
+    stack1 = aggStacks(:,:,stackidx(3)); stackcn1 = aggCon(:,:,stackidx(3));
+    stack2 = aggStacks(:,:,stackidx(2)); stackcn2 = aggCon(:,:,stackidx(2));
+    stack3 = aggStacks(:,:,stackidx(1)); stackcn3 = aggCon(:,:,stackidx(1));
+    rotstack = optAngle(stack2,stack1,'right','left',2);
+    Normvec = tangentPlane(stack1,stack1,'left',[0 0 rotstack],false);
+    figure(1)
+    newstack = [plotaggregate(stack1,stack2,stackcn1,[0 0 rotstack],Normvec,'left',true); stack2];
+    hold on
+    rotstack = optAngle(stack2,stack3,'left','right',2);
+    Normvec = tangentPlane(stack1,stack1,'right',[0 0 rotstack],false);
+    newstack = [plotaggregate(stack3,newstack,stackcn3,[0 0 rotstack],Normvec,'right',true); newstack];
+    hold on
+    plotaggregate(stack2,[],stackcn2,[],[],[],true);
+    axis equal 
+    hold off
+    
+    aggLayer(:,:,layer) = newstack;
+    aggLayCon(:,:,layer) = [stackcn3; stackcn1+306; stackcn2+306.*2];
+end
+
+%generating the entire cube...
+figure(1)
+for cube = 1:3
+    layer1 = aggLayer(:,:,1); layercn1 = aggLayCon(:,:,1);
+    layer2 = aggLayer(:,:,2); layercn2 = aggLayCon(:,:,2);
+    layer3 = aggLayer(:,:,3); layercn3 = aggLayCon(:,:,3);
+    cubestack = [plotaggregate(layer1,layer2,layercn1,[0 0 0],Normvec,'front',true); layer2];
+    hold on
+    cubestack = [plotaggregate(layer3,cubestack,layercn3,[0 0 0],Normvec,'back',true);cubestack];
+    hold on
+    plotaggregate(layer2,[],layercn2,[],[],[],true);
+    axis equal
+    hold off
+end
+
+function result = sortAgg(aggregates)
+%find the first aggregate in the center of the cube and use this as the
+%origin to compare it with other cubes...
+
+%translate each of the other cubes around that cube at the origin and store
+%the coordinates and perform an overlap check using alphashapes  along with
+%the number that each of the aggregates belong to. if there is an overlap,
+%shift the aggregate slightly back to the center of its cube and continue
+%until the overlap is done.
+
+%continue for the rest of the cubes...
+end
+
 function pts2 = plotaggregate(pts2,pts1,cnt2,rotangle,NormVec,option,figurecheck)
 if ~isempty(rotangle)
-pts2 = Rotate(pts2,0,rotangle,0);
+pts2 = Rotate(pts2,angle(1),angle(2),angle(3));
 end
 
 if ~isempty(NormVec)
@@ -11,20 +110,25 @@ pts2 = Translate(pts2,pts1,option);
 end
 
 if figurecheck == true
-trimesh(cnt2,pts2(:,1),pts2(:,2),pts2(:,3),'FaceAlpha',1,'EdgeColor','k');
+trimesh(cnt2,pts2(:,1),pts2(:,2),pts2(:,3),'FaceAlpha',1,'EdgeColor','k','FaceColor','b');
 hold on
 end
 end
 
-function result = optAngle(pts1,pts2,restrictface1,restrictface2)
+function result = optAngle(pts1,pts2,restrictface1,restrictface2,opt)
 %Inputs: agg1 - the stationary/fixed aggregate
 %             agg2 - the aggregate that needs to be "attached"/shifted
-a = linspace(0,pi/2);
+a = linspace(-pi/3,pi/3);
 optimalangle = [];
-plane1 = tangentPlane(pts1,[],restrictface1,0,false);
+plane1 = tangentPlane(pts1,[],restrictface1,[0 0 0],false);
 
 parfor ang = 1:length(a)
-    plane2 = tangentPlane(pts2,[],restrictface2,a(ang),false);
+    if opt == 2 %y-axis
+        rots = [0, a(ang), 0];
+    else 
+        rots = [0, 0, a(ang)];
+    end
+    plane2 = tangentPlane(pts2,[],restrictface2,rots,false);
     radt = anglebwPlanes(plane1,plane2);
     optimalangle = [optimalangle; radt];
 end
@@ -43,7 +147,7 @@ end
 function result = tangentPlane(pts1,cnt1,option,angle,figurecheck)
 %restrictface 1,2 - negative, 3,4 - positive
 %restrictface 1,3 - negative to 0, 2,4 - 0 to positive
-%
+
 if contains(option,'left')
     split1 = 1; %axis where the aggregate is cut into halves
     split2 = 3; %axis where the aggregate is cut into halves again
@@ -75,7 +179,7 @@ elseif contains (option, 'back')
     split3 = 1;
     restrictface = 1;
 end
-pts1 = Rotate(pts1,0,angle,0);
+pts1 = Rotate(pts1,angle(1),angle(2),angle(3));
 
 %generating the tangent line of the top...
 [~,idx] = sort(pts1(:,split1));
@@ -248,9 +352,9 @@ if contains(option,'left')
 elseif contains(option,'right')
     datapointsn(:,1) = datapointsn(:,1) + 1*(alpha+StepSz);
 elseif contains(option,'bottom')
-    datapointsn(:,3) = datapointsn(:,3) + -1*(alpha+StepSz);
-elseif contains(option,'top')
     datapointsn(:,3) = datapointsn(:,3) + 1*(alpha+StepSz);
+elseif contains(option,'top')
+    datapointsn(:,3) = datapointsn(:,3) + -1*(alpha+StepSz);
 elseif contains(option,'front')
     datapointsn(:,2) = datapointsn(:,2) + 1*(alpha+StepSz);
 elseif contains(option,'back')

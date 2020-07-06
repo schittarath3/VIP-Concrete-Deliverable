a800d80cac13a8a1bb5e29f984f38c16b865bef6
Souliya <souliya3chittarath@gmail.com>
2020-07-05 21:13:23 -0400

    Replaced aggCubeTest.m with ConcreteDriver.m

---
+++ ConcreteDriver.m
@@ -0,0 +1,12 @@
+clear
+clc
+%% Driving Code
+
+repos = generateRepo('STL Files\Aggregates Processed 3\*.stl', 9);         %Creating initial directory from stl files of aggregate
+
+startCoords = [1 1 1];                                                     %Creating cubelets to store place aggregates
+cubeSize = 561;
+nDivisions = 3;
+testCubes = genCublets(cubeSize, nDivisions, startCoords, 1);              
+
+aggRepo = insertAgg(repos, testCubes, 1, 3);                               %Inserting aggregates into cubes and generating a new repo
